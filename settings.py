c7a3f60705adeeb24165e6ec832569956d9bca74
Thomas Gruner <tom.gruner@gmail.com>
2015-03-24 21:16:20 -0700

    Update settings.py

--- settings.py
+++ settings.py
@@ -25,8 +25,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'askbot',                      # Or path to database file if using sqlite3.
-        'USER': 'askbot',                      # Not used with sqlite3.
-        'PASSWORD': 'askbot',                  # Not used with sqlite3.
+        'USER': 'globallometree',                      # Not used with sqlite3.
+        'PASSWORD': 'globallometree',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
         'TEST_CHARSET': 'utf8',              # Setting the character set and collation to utf-8
